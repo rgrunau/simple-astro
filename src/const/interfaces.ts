0c344000d85158323e6719cd7e81da40bea393c4
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-11-26 16:59:50 -0800

    a little better

--- src/const/interfaces.ts
+++ src/const/interfaces.ts
@@ -1,13 +1,15 @@
 export type ResponsiveImage = {
-  aspectRatio: number;
-  base64: string;
-  height: number;
-  sizes: string;
-  src: string;
-  srcSet: string;
-  width: number;
-  webpSrcSet: string;
-  alt: string;
+  responsiveImage: {
+    aspectRatio: number;
+    base64: string;
+    height: number;
+    sizes: string;
+    src: string;
+    srcSet: string;
+    width: number;
+    webpSrcSet: string;
+    alt: string;
+  };
 };
 
 export interface BlogPost {
